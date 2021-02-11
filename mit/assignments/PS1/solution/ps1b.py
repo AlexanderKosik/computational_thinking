8e7396f7d563a79bc44f5afe8da99ca6a2b514ea
Alexander Kosik <alexander.kosik@aareal-bank.com>
2021-02-11 18:08:06 +0100

    Solution is near

--- mit/assignments/PS1/solution/ps1b.py
+++ mit/assignments/PS1/solution/ps1b.py
@@ -10,52 +10,46 @@
 #================================
 
 # Problem 1
-def dp_make_weight(egg_weights, target_weight, memo={}):
-    """
-    Find number of eggs to bring back, using the smallest number of eggs. Assumes there is
-    an infinite supply of eggs of each weight, and there is always a egg of value 1.
-    
-    Parameters:
-    egg_weights - tuple of integers, available egg weights sorted from smallest to largest value (1 = d1 < d2 < ... < dk)
-    target_weight - int, amount of weight we want to find eggs to fit
-    memo - dictionary, OPTIONAL parameter for memoization (you may not need to use this parameter depending on your implementation)
-    
-    Returns: int, smallest number of eggs needed to make target weight
-    """
-    if not egg_weights or target_weight < 0:
-        # weight limit exceeded
-        return None
-    elif target_weight == 0:
-        # use this egg
-        return 1
+def recursion(egg_weights, target_weight, choice, memo={}):
+    if target_weight == 0 or (not bool(egg_weights)):
+        return len(choice), choice
     elif egg_weights[-1] > target_weight:
-        # right branch: do not take egg
-        return dp_make_weight(egg_weights[:-1], target_weight, memo)
+        # right branch only
+        return recursion(egg_weights[:-1], target_weight, list(choice))
     else:
-        # left branch: take egg
-        next_item = egg_weights[-1]
-        # use all egg_weights
-        left = dp_make_weight(egg_weights, target_weight - next_item)
-
-        # right branch: egg to big
-        right = dp_make_weight(egg_weights[:-1], target_weight)
-
-        if left is None:
-            return right
-        elif right is None:
-            return left
-        elif left < right:
-            return left+1
+        item = egg_weights[-1]
+        left_choice = list(choice) + [item]
+        # check if finished
+        if target_weight - item == 0:
+            return len(left_choice), left_choice
         else:
-            return right+1
+            # go deeper into rabbit hole
+            left, result = recursion(egg_weights, target_weight - item, left_choice)
 
-        
+        # when we only have 1 choice and already choose it (left), than we must use left
+        if len(egg_weights) == 1:
+            return len(result), result
+
+        right_choice = list(choice)
+        right, result = recursion(egg_weights[:-1], target_weight, right_choice)
+
+        if left <= right:
+            result = choice + left_choice
+        else:
+            result = choice + right_choice
+
+        return len(result), result
+
+def dp_make_weight(egg_weights, target_weight, memo={}):
+    length, result = recursion(egg_weights, target_weight, [], memo)
+    print(result)
+    return length
 
 # EXAMPLE TESTING CODE, feel free to add more if you'd like
 if __name__ == '__main__':
     # egg_weights = (1, 5, 10, 25)
-    egg_weights = (1, 5)
-    n = 6
+    egg_weights = (1, 2)
+    n = 3
     print(f"Egg weights = {egg_weights}")
     print(f"n = {n}")
     # print("Expected ouput: 9 (3 * 25 + 2 * 10 + 4 * 1 = 99)")
